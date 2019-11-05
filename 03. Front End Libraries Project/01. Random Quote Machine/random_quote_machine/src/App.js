965fcea51221f2daeb1f0ab204833e1d5606229c
John Lau <nohjlau@gmail.com>
2019-11-04 18:20:14 -0800

    Implement tutorial todolist

--- 03. Front End Libraries Project/01. Random Quote Machine/random_quote_machine/src/App.js
+++ 03. Front End Libraries Project/01. Random Quote Machine/random_quote_machine/src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Header from './components/layout/Header'
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 import './App.css';
+import uuid from 'uuid';
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take our trash',
+        completed: true
+      },
+      {
+        id: 2,
+        title: 'Run a mile',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Cook dinner',
+        completed: false
+      }
+    ]
+  };
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map(todo => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    }) });
+    console.log(id);
+  }
 
-function App() {
+  delTodo = (id) => {
+    this.setState({ todos: this.state.todos.filter(todo => { return todo.id !== id; } 
+      )})
+  }
+
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuid.v4(),
+      title, //title: title
+      completed: false,
+    }
+    this.setState({ todos: [...this.state.todos, newTodo]})
+  }
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Header />
+        <AddTodo addTodo={this.addTodo} />
+        <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+      </div>
     </div>
   );
+  }
 }
 
 export default App;
