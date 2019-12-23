a0db7b806d76350bce202c25f2040209c5f8ea47
John Lau <nohjlau@gmail.com>
2019-12-23 14:44:23 -0800

    Algorithms: Optimize dup check for SymDiff

--- 07. Coding Interview Prep/Algorithms/01. Find the Symmetric Difference.js
+++ 07. Coding Interview Prep/Algorithms/01. Find the Symmetric Difference.js
@@ -9,34 +9,23 @@ function sym() {
             c = findSymDiff(c, arguments[i]);
         }
     }
-    let finalArr = [];
-    // Remove duplicates
-    c.map(d => {
-        if (finalArr.indexOf(d) == -1) { finalArr.push(d)}
-    });
-    return finalArr;
+    
+    return c;
 }
 
 function findSymDiff(a, b) {
     let finalArray = [];
     a.map(d => {
-        if(b.indexOf(d) == -1) {
+        if(b.indexOf(d) == -1 && finalArray.indexOf(d) == -1) { // Check for symdif & not a dup
             finalArray.push(d);
         }
     });
 
     b.map(d => {
-        if(a.indexOf(d) == -1) {
+        if(a.indexOf(d) == -1 && finalArray.indexOf(d) == -1) {
             finalArray.push(d);
         }
     })
 
     return finalArray.sort((a,b) => a-b);
 }
-
-
-a = [1,2,3];
-b = [2,3,4];
-c = [4,5,6];
-
-console.log(sym([1, 2, 3, 3], [5, 2, 1, 4]));
