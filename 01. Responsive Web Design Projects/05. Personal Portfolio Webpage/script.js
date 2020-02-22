efc8db0e8a25e8b761a70d825e11275c4c479932
John Lau <nohjlau@gmail.com>
2020-02-22 12:56:12 -0800

    Reorganizing repository

---
+++ 01. Responsive Web Design Projects/05. Personal Portfolio Webpage/script.js
@@ -0,0 +1,7 @@
+var projectOne = {
+    title: "Test",
+}
+function createProject(infoObj, className) {
+    let cats = document.getElementsByClassName('project-tile')[0].innerHTML = "test";
+    console.log(cats);
+}
